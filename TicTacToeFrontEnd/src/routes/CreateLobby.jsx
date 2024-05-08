7a2d0802f4bffa4be4257720b11c8fdab918dd79
Mingmar Gyalzen Sherpa <mingmagsherpa6@gmail.com>
2024-05-08 15:37:37 +0545

    added handler for input

--- TicTacToeFrontEnd/src/routes/CreateLobby.jsx
+++ TicTacToeFrontEnd/src/routes/CreateLobby.jsx
@@ -1,21 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function CreateLobby() {
+  const [lobbyName, setLobbyName] = useState("");
+  const [password, setPassword] = useState("");
+  const [err, setErr] = useState("");
+  const handleNameChange = (e) => {
+    setLobbyName(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
   return (
     <div className="create-lobby">
       <form className="form-card">
         <h2>Create a Lobby</h2>
+        <p className="err">{err}</p>
         <div className="input-wrapper">
-          <input type="password" placeholder="Enter your lobby name" />
+          <input
+            type="text"
+            onChange={handleNameChange}
+            placeholder="Enter your lobby name"
+          />
         </div>
         <div className="input-wrapper">
-          <input type="password" placeholder="Enter your lobby password" />
+          <input
+            type="password"
+            onChange={handlePasswordChange}
+            placeholder="Enter your lobby password"
+          />
         </div>
         <p>Note: Leave password empty to create a free to join game</p>
-        <Link to={"/"} className="btn w-100 mt-40">
-          CREATE
-        </Link>
+        {lobbyName == "" ? (
+          <Link
+            to={"/"}
+            onClick={(e) => {
+              setErr("Lobby name can't be empty");
+              e.preventDefault();
+            }}
+            className="btn w-100 mt-40"
+          >
+            CREATE
+          </Link>
+        ) : (
+          <Link to={"/"} className="btn w-100 mt-40">
+            CREATE
+          </Link>
+        )}
       </form>
     </div>
   );
--- TicTacToeFrontEnd/src/routes/JoinLobby.jsx
+++ TicTacToeFrontEnd/src/routes/JoinLobby.jsx
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function JoinLobby() {
+  const [lobbyName, setLobbyName] = useState("");
+  const [err, setErr] = useState("");
+
+  function handleNameChange(e) {
+    setLobbyName(e.target.value);
+  }
+  console.log(lobbyName);
   return (
     <div className="create-lobby">
       <form className="form-card">
         <h2>Join a Lobby</h2>
+        <p className="err">{err}</p>
         <div className="input-wrapper">
-          <input type="password" placeholder="Enter lobby name" />
+          <input
+            onChange={handleNameChange}
+            onFocus={() => setErr("")}
+            type="text"
+            placeholder="Enter lobby name"
+          />
         </div>
         <div className="input-wrapper">
           <input type="password" placeholder="Enter lobby password" />
         </div>
 
-        <Link to={"/"} className="btn w-100 mt-40">
-          CREATE
-        </Link>
+        {lobbyName == "" ? (
+          <Link
+            to={"/"}
+            onClick={(e) => {
+              e.preventDefault();
+              setErr("Lobby name can't be empty");
+            }}
+            className="btn w-100 mt-40"
+          >
+            CREATE
+          </Link>
+        ) : (
+          <Link to={"/"} className="btn w-100 mt-40">
+            CREATE
+          </Link>
+        )}
       </form>
     </div>
   );
